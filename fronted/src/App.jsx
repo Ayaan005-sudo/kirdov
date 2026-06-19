73a6dbfb9c56f599566ab04711cfcf03a0da4a5a
Ayaan Ahmed Abbasi <ayaanah776@gmail.com>
2026-06-19 19:36:51 +0530

    name change

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
-import WorkshopDetails from './components/WorkShopdetails';
+import WorkshopDetails from './components/WorkShopDetails';
 import LearningOutcomes from './components/LearningOutcomes';
 import FAQSection from './components/FAQSection';
 import RegistrationForm from './components/RegistrationForm';
