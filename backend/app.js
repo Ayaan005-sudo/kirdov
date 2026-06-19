0d880757c97dfbb8402c436f7017a43f90880b39
Ayaan Ahmed Abbasi <ayaanah776@gmail.com>
2026-06-19 19:47:00 +0530

    backend ready for deploy

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const userModel = require("./model/userSchema");
 const cors = require('cors');
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://kirdov.vercel.app"
 }));
 
 async function main(){
